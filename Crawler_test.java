669e935ca49e5e2591c23b05a0e0982533dd48ed
黃瀚緯 <huanghanwei@huanghanweide-MacBook-Pro.local>
2017-06-09 09:38:26 +0800

    Crawler 4.0
    所有七間銀行匯率資料擷取完成
    (台灣 兆豐 玉山 華南 渣打 匯豐 台新)

--- Crawler_test.java
+++ Crawler_test.java
@@ -2,13 +2,13 @@ package final_project;
 
 import java.io.*;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Iterator;
+//import java.net.URLConnection;
+//import java.util.Iterator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.parser.Parser;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 
 public class Crawler_test {
@@ -22,8 +22,11 @@ public class Crawler_test {
 	private static double[] mega = new double[14]; //兆豐銀行
 	private static double[] esun = new double[14]; //玉山銀行
 	private static double[] hncb = new double[14]; // 華南銀行
- 	
-	public static void main(String[] args) throws Exception{
+	private static double[] scb = new double[14]; // 渣打銀行
+ 	private static double[] hsbc = new double[14]; // 匯豐銀行
+ 	private static double[] tsb = new double[14]; // 台新銀行
+	
+	public static void main(String[] args) throws IOException{
 		
 		buyORsell[0] = "現金買入";
 		buyORsell[1] = "現金賣出";
@@ -42,6 +45,9 @@ public class Crawler_test {
 		URL mega_web = new URL("http://www.findrate.tw/bank/5/#.WTZwjROGOt8"); //兆豐銀行
 		URL esun_web = new URL("https://www.esunbank.com.tw/bank/Layouts/esunbank/Accessibility/rate_exchange.aspx"); //玉山銀行
 		URL hncb_web = new URL("https://ibank.hncb.com.tw/netbank/pages/jsp/ExtSel/RTExange.html"); //華南銀行
+		URL scb_web = new URL("http://www.standardchartered.com.tw/check/inquiry-rate-foreign-exchange.asp"); //渣打銀行
+		URL hsbc_web = new URL("https://www.hsbc.com.tw/1/2/Misc/popup-tw/currency-calculator"); //匯豐銀行
+		URL tsb_web = new URL("http://www.findrate.tw/bank/9/#.WTjyIBN97-Y"); //台新銀行
 		
 		/*
          * 向網頁伺服發出請求，並將回應分析成document。
@@ -49,10 +55,13 @@ public class Crawler_test {
          * 第二個參數是：連線時間(毫秒)，在指定時間內若無回應則會丟出IOException
          */
 		
-        Document bot_doc = Jsoup.parse(bot_web, 3000); // bot: 台灣銀行
-        Document mega_doc = Jsoup.parse(mega_web, 3000); // mega: 兆豐銀行
-        Document esun_doc = Jsoup.parse(esun_web, 3000); // esun: 玉山銀行
-        Document hncb_doc = Jsoup.parse(hncb_web, 3000); // hncb: 華南銀行
+        Document bot_doc = Jsoup.parse(bot_web, 4000); // bot: 台灣銀行
+        Document mega_doc = Jsoup.parse(mega_web, 4000); // mega: 兆豐銀行
+        Document esun_doc = Jsoup.parse(esun_web, 4000); // esun: 玉山銀行
+        Document hncb_doc = Jsoup.parse(hncb_web, 4000); // hncb: 華南銀行
+        Document scb_doc = Jsoup.parse(scb_web, 4000); // scb: 渣打銀行
+        Document hsbc_doc = Jsoup.parse(hsbc_web, 4000); // hsbc:匯豐銀行
+        Document tsb_doc = Jsoup.parse(tsb_web, 4000); // tsb: 台新銀行
         
         /*String bot_title = bot.title(); // 抓取網頁標題
         String mega_title = sinopac.title();
@@ -136,21 +145,82 @@ public class Crawler_test {
         hncb[12] = Double.parseDouble(hncb_elements.get(26).text()); //華南銀行英鎊現金買入
         hncb[13] = Double.parseDouble(hncb_elements.get(27).text()); //華南銀行英鎊現金賣出
         
-        for(int i = 0; i < hncb.length; i++){
+        /*for(int i = 0; i < hncb.length; i++){
         	System.out.println(hncb[i]);
-        }
+        }*/
         
-        //System.out.println(bot_elements.size()); 
+        Elements scb_elements = scb_doc.select("td.talign_c");
+        scb[0] = Double.parseDouble(scb_elements.get(0).text()); //渣打銀行美金現金買入
+        scb[1] = Double.parseDouble(scb_elements.get(1).text()); //渣打銀行美金現金賣出
+        scb[2] = Double.parseDouble(scb_elements.get(4).text()); //渣打銀行港幣現金買入
+        scb[3] = Double.parseDouble(scb_elements.get(5).text()); //渣打銀行港幣現金賣出
+        scb[4] = Double.parseDouble(scb_elements.get(28).text()); //渣打銀行日圓現金買入
+        scb[5] = Double.parseDouble(scb_elements.get(29).text()); //渣打銀行日圓現金賣出
+        //scb[6] = Double.parseDouble(scb_elements.get(52).text()); //渣打銀行韓元現金買入
+        //scb[7] = Double.parseDouble(scb_elements.get(53).text()); //渣打銀行韓元現金賣出
+        scb[8] = Double.parseDouble(scb_elements.get(36).text()); //渣打銀行歐元現金買入
+        scb[9] = Double.parseDouble(scb_elements.get(37).text()); //渣打銀行歐元現金賣出
+        scb[10] = Double.parseDouble(scb_elements.get(52).text()); //渣打銀行人民幣現金買入
+        scb[11] = Double.parseDouble(scb_elements.get(53).text()); //渣打銀行人民幣現金賣出
+        scb[12] = Double.parseDouble(scb_elements.get(8).text()); //渣打銀行英鎊現金買入
+        scb[13] = Double.parseDouble(scb_elements.get(9).text()); //渣打銀行英鎊現金賣出*/
+        //渣打銀行無韓元現金匯兌
+        
+        /*for(int i = 0; i < scb.length; i++){
+        	System.out.println(scb[i]);
+        }*/
+        
+        Elements hsbc_elements = hsbc_doc.select("td.ForRatesColumn02");
+        hsbc[0] = Double.parseDouble(hsbc_elements.get(3).text()); //匯豐銀行美金現金買入
+        hsbc[1] = Double.parseDouble(hsbc_elements.get(4).text()); //匯豐銀行美金現金賣出
+        hsbc[2] = Double.parseDouble(hsbc_elements.get(28).text()); //匯豐銀行港幣現金買入
+        hsbc[3] = Double.parseDouble(hsbc_elements.get(29).text()); //匯豐銀行港幣現金賣出
+        hsbc[4] = Double.parseDouble(hsbc_elements.get(48).text()); //匯豐銀行日圓現金買入
+        hsbc[5] = Double.parseDouble(hsbc_elements.get(49).text()); //匯豐銀行日圓現金賣出
+        //hsbc[6] = Double.parseDouble(hsbc_elements.get(52).text()); //匯豐銀行韓元現金買入
+        //hsbc[7] = Double.parseDouble(hsbc_elements.get(53).text()); //匯豐銀行韓元現金賣出
+        hsbc[8] = Double.parseDouble(hsbc_elements.get(8).text()); //匯豐銀行歐元現金買入
+        hsbc[9] = Double.parseDouble(hsbc_elements.get(9).text()); //匯豐銀行歐元現金賣出
+        hsbc[10] = Double.parseDouble(hsbc_elements.get(63).text()); //匯豐銀行人民幣現金買入
+        hsbc[11] = Double.parseDouble(hsbc_elements.get(64).text()); //匯豐銀行人民幣現金賣出
+        hsbc[12] = Double.parseDouble(hsbc_elements.get(13).text()); //匯豐銀行英鎊現金買入
+        hsbc[13] = Double.parseDouble(hsbc_elements.get(14).text()); //匯豐銀行英鎊現金賣出
+        // 匯豐銀行無韓元匯兌
+        
+        /*for(int i = 0; i < hsbc.length; i++){
+        	System.out.println(hsbc[i]);
+        }*/
+        
+        Elements tsb_elements = tsb_doc.select("td.wordB");
+        tsb[0] = Double.parseDouble(tsb_elements.get(48).text()); //台新銀行美金現金買入
+        tsb[1] = Double.parseDouble(tsb_elements.get(49).text()); //台新銀行美金現金賣出
+        tsb[2] = Double.parseDouble(tsb_elements.get(24).text()); //台新銀行港幣現金買入
+        tsb[3] = Double.parseDouble(tsb_elements.get(25).text()); //台新銀行港幣現金賣出
+        tsb[4] = Double.parseDouble(tsb_elements.get(28).text()); //台新銀行日圓現金買入
+        tsb[5] = Double.parseDouble(tsb_elements.get(29).text()); //台新銀行日圓現金賣出
+        //tsb[6] = Double.parseDouble(tsb_elements.get(52).text()); //台新銀行韓元現金買入
+        //tsb[7] = Double.parseDouble(tsb_elements.get(53).text()); //台新銀行韓元現金賣出
+        tsb[8] = Double.parseDouble(tsb_elements.get(16).text()); //台新銀行歐元現金買入
+        tsb[9] = Double.parseDouble(tsb_elements.get(17).text()); //台新銀行歐元現金賣出
+        tsb[10] = Double.parseDouble(tsb_elements.get(12).text()); //台新銀行人民幣現金買入
+        tsb[11] = Double.parseDouble(tsb_elements.get(13).text()); //台新銀行人民幣現金賣出
+        //tsb[12] = Double.parseDouble(tsb_elements.get(48).text()); //台新銀行英鎊現金買入
+        //tsb[13] = Double.parseDouble(tsb_elements.get(49).text()); //台新銀行英鎊現金賣出 
+        //台新銀行無韓元匯兌, 無英鎊現金匯兌
+        /*for(int i = 0; i < tsb.length; i++){
+        	System.out.println(tsb[i]);
+        }
+        /*for(int i = 0; i < 72; i++){
+        	System.out.println(tsb_elements.get(i).text());
+        } 依序輸出所選取tag之所有元素*/
+        
+        //System.out.println(bot_elements.size());  求該tag數量方便確認
         //System.out.println(mega_elements.size());
         //System.out.println(esun_elements.size()); 
         //System.out.println(hncb_elements.size()); 
-       
-        //System.out.println(mega_elements.get(0).text()); 
-       
-        
-        /*for(int i = 0; i < 20; i++){
-        	System.out.println(bot_elements.get(i).text());  
-        }*/
-        	
+        //System.out.println(scb_elements.size()); 
+        //System.out.println(hsbc_elements.size()); 
+        //System.out.println(tsb_elements.size()); 
+                	
 	}
 }
